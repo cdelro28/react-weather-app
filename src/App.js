60bafcc4d9b0b1e10c73262b514d42836f19c8fc
cdelro28 <149708117+cdelro28@users.noreply.github.com>
2025-02-27 12:53:34 -0500

    Added footer with source links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,34 @@ export default function App() {
   return (
     <div className="App">
       <WeatherApp />
+      <footer>
+        This project was coded by
+        <a
+          href="https://www.linkedin.com/in/christinedelrosario/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Christine Del Rosario{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/cdelro28/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://cd-react-weather-app.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
