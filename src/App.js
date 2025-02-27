688b763f2d7d45d6ad892864fa0f30e31d58b608
cdelro28 <149708117+cdelro28@users.noreply.github.com>
2025-02-26 21:23:30 -0500

    Add footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,34 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <WeatherSearch />
+      <footer>
+        This project was coded by
+        <a
+          href="https://www.linkedin.com/in/christinedelrosario/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Christine Del Rosario{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/cdelro28/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://cd-react-weather-app.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
