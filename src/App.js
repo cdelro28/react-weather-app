2abddd555f61b6dd1fa87d5715024d732d786cc7
cdelro28 <149708117+cdelro28@users.noreply.github.com>
2025-03-16 14:13:45 -0400

    updated default city

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <WeatherApp defaultCity="Texas" />
+        <WeatherApp defaultCity="Boston" />
 
         <footer>
           This project was coded by
